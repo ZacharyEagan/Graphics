2e65555660f22f5a4211098d3ecc1968b475f3ae
Thinh Luu <tpluu@calpoly.edu>
2017-09-18 17:39:47 -0700

    Wrote Triangle class and draw methods for all.

--- lab01-master/src/main.cpp
+++ lab01-master/src/main.cpp
@@ -9,6 +9,48 @@
 // You should never do this in a header file.
 using namespace std;
 
+
+
+
+
+class my_pixel
+{
+public:
+	my_pixel(int x = 0, int y = 0, int z = 0, int r = 255, int g = 255, int b = 255);
+	void draw(shared_ptr<Image> img);
+private:
+	int _x, _y, _z;
+	int _r, _g, _b;
+	
+};
+
+my_pixel::my_pixel(int x, int y, int z, int r, int g, int b) : _x(x), _y(y), _z(z), _r(r), _g(g), _b(b) {}
+void my_pixel::draw(shared_ptr<Image> img)
+{
+	img->setPixel(_x, _y, _r, _g, _b);
+}
+
+
+//////////////////////////
+class my_triangle
+{
+public:
+	my_triangle(my_pixel a, my_pixel b, my_pixel c);
+	void draw(shared_ptr<Image> img);
+private:
+	my_pixel _a, _b, _c;
+};
+
+my_triangle::my_triangle(my_pixel a, my_pixel b, my_pixel c) : _a(a), _b(b), _c(c) {}
+void my_triangle::draw(shared_ptr<Image> img)
+{
+	_a.draw(img);
+	_b.draw(img);
+	_c.draw(img);
+}
+
+
+
 int main(int argc, char **argv)
 {
 	if(argc < 4) {
@@ -33,6 +75,11 @@ int main(int argc, char **argv)
 	int x3 = atoi(argv[8]);
 	int y3 = atoi(argv[9]);
 	
+	my_pixel a(x1, y2);
+	my_pixel b(x2, y2);
+	my_pixel c(x3, y3);
+
+	my_triangle tri(a, b, c);
 
 	// Create the image. We're using a `shared_ptr`, a C++11 feature.
 	auto image = make_shared<Image>(width, height);
@@ -45,6 +92,10 @@ int main(int argc, char **argv)
 			image->setPixel(x, y, r, g, b);
 		}
 	}
+
+	tri.draw(image);
+	
+
 	// Write image to file
 	image->writeToFile(filename);
 	return 0;
